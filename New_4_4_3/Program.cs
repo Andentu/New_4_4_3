4e0659b710cfd709be95c117845720b2722aab4d
Andentu <severalmaz97@gmail.com>
2023-02-16 20:40:07 +0300

    Добавлен код иной записи кортеджа

--- New_4_4_3/Program.cs
+++ New_4_4_3/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
+            var (name, age) = ("Артур", 16);
+            Console.WriteLine("Your name is {0} and your age is {1}", name, age);
+            Console.WriteLine("Enter your name: ");
+           
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            age = int.Parse(Console.ReadLine());
             
+            Console.WriteLine("Your name is {0} and your age is {1}", name, age);
+            Console.ReadKey();
         }
     }
 }
